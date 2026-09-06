068f29b3ef76677cc456ebb30b0a56a2b4efea65
Pratyush Priyadarshan <priyadarshanpratyush1125@gmail.com>
2026-09-06 20:18:31 +0530

    update lexical-sort.c

--- lexical-sort.c
+++ lexical-sort.c
@@ -11,10 +11,31 @@ int main()
 
     //Declare num number of strings with available length of 100 characters.
     char word[num][100];
+    //Input strings one by one.
+    for(int j=0;j<num;j++)
+    {
+        printf("Enter word %d : ",j+1);
+        scanf("\n%[^\n]",word[j]); //include white spaces
+    }
 
+    //Bubble sort algorithm
+    for(int i=0;i<num-1;i++)
+        for(int j=0;j<num-1;j++)
+        {
+            //Compare two strings using strcmp.
+            //If 1st string is greater than 2nd then swap strings.
+            if(strcmp(word[j],word[j+1])>0)
+            {
+                char temp[100]; //Temporary string
+                strcpy(temp,word[j]);
+                strcpy(word[j],word[j+1]);
+                strcpy(word[j+1],temp);
+            }
+        }
 
-
-
-
+    //Print arranged strings one by one.
+    printf("\nArranging in dictionary order :\n\n");
+    for(int j=0;j<num;j++)
+        printf("%s\n",word[j]);
         return 0;
 }

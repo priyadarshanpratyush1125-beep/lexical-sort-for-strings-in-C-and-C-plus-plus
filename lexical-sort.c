22902271599950e7c49480a0dc2a3f34881659c4
Pratyush Priyadarshan <priyadarshanpratyush1125@gmail.com>
2026-09-06 20:17:17 +0530

    update lexical-sort.c

---
+++ lexical-sort.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    //Get number of strings to arrange.
+    printf("Enter number of words you want to arrange : ");
+    int num;
+    scanf("%d",&num);
+
+    //Declare num number of strings with available length of 100 characters.
+    char word[num][100];
+
+
+
+
+
+        return 0;
+}
